47ed6acd2a00a12fe2968be55822879f099a8748
ParkHyunsu1110 <phs0767@naver.com>
2021-04-01 20:46:49 +0900

    그래프 1차 제작

--- workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Home.java
+++ workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Home.java
@@ -2,14 +2,27 @@ package com.capstonedesign.lovebaby;
 
 import android.app.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Home extends Activity {
+    Button graphBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        graphBtn = findViewById(R.id.graphBtn);
+
+        graphBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Graph.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/MainActivity.java
+++ workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/MainActivity.java
@@ -1,13 +1,53 @@
 package com.capstonedesign.lovebaby;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    //선언
+    Button entranceBtn, infoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //할당
+        entranceBtn = findViewById(R.id.entranceBtn);
+        infoBtn = findViewById(R.id.infoBtn);
+
+        //로그인 이후 입장 버튼
+        entranceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            }
+        });
+
+        //제작자 정보
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.info);
+                dialog.setTitle("Programmers Info");
+
+                Button okBtn = dialog.findViewById(R.id.okBtn);
+
+                okBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+
     }
 }
