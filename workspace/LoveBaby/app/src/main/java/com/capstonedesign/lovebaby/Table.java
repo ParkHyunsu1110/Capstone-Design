835ed6d69e6e86c42a090a77cb38fb5f5197b067
DESKTOP-GK2TVGV\HOME <dmlwjd558@gmail.com>
2021-05-10 14:04:27 +0900

    표 수정 및 업데이트

--- workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Table.java
+++ workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Table.java
@@ -1,14 +1,65 @@
 package com.capstonedesign.lovebaby;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Table extends Activity {
+
+    //변수 모음
+    TextView weightText_1, heightText_1;
+    TextView weightText_2, heightText_2;
+    TextView weightText_3, heightText_3;
+    TextView weightText_4, heightText_4;
+    TextView weightText_5, heightText_5;
+
     //표 제작 예정
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.table);  //현재 사용 xml = table.xml
 
+        //테이블 몸무게, 키 TextView 변수 할당
+        weightText_1 = findViewById(R.id.weightText_1); heightText_1 = findViewById(R.id.heightText_1);
+        weightText_2 = findViewById(R.id.weightText_2); heightText_2 = findViewById(R.id.heightText_2);
+        weightText_3 = findViewById(R.id.weightText_3); heightText_3 = findViewById(R.id.heightText_3);
+        weightText_4 = findViewById(R.id.weightText_4); heightText_4 = findViewById(R.id.heightText_4);
+        weightText_5 = findViewById(R.id.weightText_5); heightText_5 = findViewById(R.id.heightText_5);
+
+        //인텐트로 값 할당
+        Intent getIntent = getIntent();
+        String weightText = getIntent.getExtras().getString("cm");
+        String heightText = getIntent.getExtras().getString("kg");
+        String monthInput = getIntent.getExtras().getString("month");
+
+        switch (Integer.parseInt(monthInput)) {
+            case 1:
+                weightText_1.setText(weightText);
+                heightText_1.setText(heightText);
+                break;
+            case 2:
+            case 3:
+                weightText_2.setText(weightText);
+                heightText_2.setText(heightText);
+                break;
+            case 4:
+            case 5:
+                weightText_3.setText(weightText);
+                heightText_3.setText(heightText);
+                break;
+            case 6:
+            case 7:
+            case 8:
+                weightText_4.setText(weightText);
+                heightText_4.setText(heightText);
+                break;
+            case 9:
+            case 10:
+            case 11:
+                weightText_5.setText(weightText);
+                heightText_5.setText(heightText);
+                break;
+        }
     }
 }
