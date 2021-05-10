4b298a8eee37bab8f94298e42f148eb68edab6f0
ParkHyunsu1110 <phs0767@naver.com>
2021-05-10 20:52:50 +0900

    주석 추가, 그래프 내용 변경, 테이블 부분 버튼 추가

--- workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Graph.java
+++ workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Graph.java
@@ -1,16 +1,10 @@
 package com.capstonedesign.lovebaby;
 
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,26 +13,13 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Graph extends AppCompatActivity {
-    /**
-     * weight_1,2,3
-     * height_1,2,3
-     *  위 6가지는 우선 제외하고 진행하도록 할게요.
-     * 교수님은 우선적으로 실행을 보시는것 같으니 임의값 집어 넣고 실행하도록 하겠습니다.
-     * */
 
     //변수 모음
-    /*
-    EditText weight_1, height_1;
-    EditText weight_2, height_2;
-    EditText weight_3, height_3;*/
-    EditText weight_4, height_4;
+    TextView txtResult;
 
-    Button inputButton, outputButton;
-
-    RadioGroup radioGroup;
-    RadioButton radioBoyBtn, radioGirlBtn;
-
-    ImageView image;
+    Button inputWeightButton, outputWeightButton;
+    Button inputHeightButton, outputHeightButton;
+    Button btnBack;
 
     String cm,kg,month2;
 
@@ -48,137 +29,118 @@ public class Graph extends AppCompatActivity {
         setContentView(R.layout.graphshow);     //현재 사용 xml = graphshow.xml
 
         //그래프 생성 및 할당
-        final GraphView graph = (GraphView) findViewById(R.id.graph);
-        graph.setVisibility(View.VISIBLE);  //그래프 보이도록 설정
+        final GraphView graphWeight = (GraphView) findViewById(R.id.graphWeight);
+        graphWeight.setVisibility(View.VISIBLE);  //그래프 보이도록 설정
 
-        //몸무게, 키 editText 변수 할당
-       /* weight_1 = findViewById(R.id.weight_1); height_1 = findViewById(R.id.height_1);
-        weight_2 = findViewById(R.id.weight_2); height_2 = findViewById(R.id.height_2);
-        weight_3 = findViewById(R.id.weight_3); height_3 = findViewById(R.id.height_3);*/
-        weight_4 = findViewById(R.id.weight_4); height_4 = findViewById(R.id.height_4);
+        //그래프 생성 및 할당
+        final GraphView graphHeight = (GraphView) findViewById(R.id.graphHeight);
+        graphHeight.setVisibility(View.VISIBLE);  //그래프 보이도록 설정
 
         //실행 버튼 할당
-        inputButton = findViewById(R.id.inputButton);
-        outputButton = findViewById(R.id.outputButton);
+        inputHeightButton = findViewById(R.id.inputHeightButton);
+        outputHeightButton = findViewById(R.id.outputHeightButton);
 
-        //라디오 변수 할당
-        radioGroup = findViewById(R.id.radioGroup);
-        radioBoyBtn = findViewById(R.id.radioBoyBtn);
-        radioGirlBtn = findViewById(R.id.radioGirlBtn);
+        inputWeightButton = findViewById(R.id.inputWeightButton);
+        outputWeightButton = findViewById(R.id.outputWeightButton);
 
-        //이미지 할당 -> 추후 표로 변경 후 제거 예정
-        image = findViewById(R.id.image);
+        btnBack = findViewById(R.id.btnBack);
 
-        //아이들 표 나오도록, 선택 -> 사진이 잘 안보인다는 교수님 의견 반영하여 표로 제작할 예정임.
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                int rb = radioGroup.getCheckedRadioButtonId();
+        //텍스트뷰 할당
+        txtResult = findViewById(R.id.txtResult);
 
-                //라디오 버튼 클릭시 이미지가 보이도록하며, 라디오 그룹에 있는 버튼 중 하나만 선택 가능한 성질을 이용, 남아 선택 시 남아만, 여아 선택 시 여아만
-                switch(rb){
-                    case R.id.radioBoyBtn:
-                        image.setImageResource(R.drawable.malehwtable);
-                        image.setVisibility(View.VISIBLE);
-                        break;
-                    case R.id.radioGirlBtn:
-                        image.setImageResource(R.drawable.femalehwtable);
-                        image.setVisibility(View.VISIBLE);
-                        break;
-                }
-            }
-        });
-
-        //입력값 출력 버튼
-        inputButton.setOnClickListener(new View.OnClickListener() {
+        //신장 입력값 출력 버튼
+        inputHeightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //문자열 변수 생성
-                /*String weightInput_1, heightInput_1;
-                String weightInput_2, heightInput_2;
-                String weightInput_3, heightInput_3;*/
-                String weightInput_4, heightInput_4;
-
-                //입력된 숫자 문자열로 변경
-                /*weightInput_1 = weight_1.getText().toString(); heightInput_1 = height_1.getText().toString();
-                weightInput_2 = weight_2.getText().toString(); heightInput_2 = height_2.getText().toString();
-                weightInput_3 = weight_3.getText().toString(); heightInput_3 = height_3.getText().toString();*/
-                weightInput_4 = weight_4.getText().toString(); heightInput_4 = height_4.getText().toString();
-
                 //그래프 그리기
                 //문자열로 변경 된 입력 값을 valueOf를 사용하여 문자열에 대한 원시데이터형을 리턴하도록함.
                 //valueOf의 경우 자바스크립트 내부적으로 사용되고, 작성할 때 코드상으로는 사용되지 않는다.
                 try {
                     LineGraphSeries<DataPoint> series = new LineGraphSeries < > (new DataPoint[] {
                             //그래프 값 입력 시 오름차순 입력할 것, 그렇지 않을 경우 예외처리에 걸려서 오류 메시지 토스트 형식으로 출력됨
-                            new DataPoint(4.4, 55.0),
-                            new DataPoint(Double.valueOf("6.5"), Double.valueOf("62")),
-                            new DataPoint(Double.valueOf("7.7"), Double.valueOf("66.3")),
-                            new DataPoint(Double.valueOf("8.7"), Double.valueOf("71.5")),
-                            new DataPoint(Double.valueOf("9.6"), Double.valueOf("76.3")),
-                            new DataPoint(Double.valueOf(kg), Double.valueOf(cm))
+                            new DataPoint(0, 55.4),
+                            new DataPoint(Double.valueOf("1"), Double.valueOf("56.8")),
+                            new DataPoint(Double.valueOf("2"), Double.valueOf("58.7")),
+                            new DataPoint(Double.valueOf("3"), Double.valueOf("62.8")),
+                            new DataPoint(Double.valueOf("4"), Double.valueOf("65.7")),
+                            new DataPoint(Double.valueOf(month2), Double.valueOf(cm))
                     });
-                    graph.addSeries(series);
+                    graphHeight.addSeries(series);
                 } catch (IllegalArgumentException e) {
                     //예외처리 하여 메시지 출력하도록.
                     Toast.makeText(Graph.this, e.getMessage(), Toast.LENGTH_LONG).show();
                 }
-
-
             }
-
         });
 
-        //저장값 출력 버튼
-        outputButton.setOnClickListener(new View.OnClickListener() {
+        //신장 저장값 출력 버튼
+        outputHeightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 LineGraphSeries<DataPoint> series = new LineGraphSeries < > (new DataPoint[] {
-
-                        /*new DataPoint(Double.valueOf(weightInput_1), Double.valueOf(heightInput_1)),
-                        new DataPoint(Double.valueOf(weightInput_2), Double.valueOf(heightInput_2)),
-                        new DataPoint(Double.valueOf(weightInput_3), Double.valueOf(heightInput_3)),*/
-
                         //그래프 값 입력 시 오름차순 입력할 것, 그렇지 않을 경우 예외처리에 걸려서 오류 메시지 토스트 형식으로 출력됨
-                        new DataPoint(4.4, 55.0),
-                        new DataPoint(Double.valueOf("6.5"), Double.valueOf("62")),
-                        new DataPoint(Double.valueOf("7.7"), Double.valueOf("66.3")),
-                        new DataPoint(Double.valueOf("8.7"), Double.valueOf("71.5")),
-                        new DataPoint(Double.valueOf("9.6"), Double.valueOf("76.3"))
+                        new DataPoint(0, 55.4),
+                        new DataPoint(Double.valueOf("1"), Double.valueOf("56.8")),
+                        new DataPoint(Double.valueOf("2"), Double.valueOf("58.7")),
+                        new DataPoint(Double.valueOf("3"), Double.valueOf("62.8")),
+                        new DataPoint(Double.valueOf("4"), Double.valueOf("65.7"))
                 });
-                graph.addSeries(series);
+                graphHeight.addSeries(series);
+            }
+        });
+
+        //체중 입력값 출력 버튼
+        inputWeightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    LineGraphSeries<DataPoint> series = new LineGraphSeries < > (new DataPoint[] {
+                            //그래프 값 입력 시 오름차순 입력할 것, 그렇지 않을 경우 예외처리에 걸려서 오류 메시지 토스트 형식으로 출력됨
+                            new DataPoint(0, 4.4),
+                            new DataPoint(Double.valueOf("1"), Double.valueOf("6.5")),
+                            new DataPoint(Double.valueOf("2"), Double.valueOf("7.0")),
+                            new DataPoint(Double.valueOf("3"), Double.valueOf("7.4")),
+                            new DataPoint(Double.valueOf("4"), Double.valueOf("7.8")),
+                            new DataPoint(Double.valueOf(month2), Double.valueOf(kg))
+                    });
+                    graphWeight.addSeries(series);
+                } catch (IllegalArgumentException e) {
+                    //예외처리 하여 메시지 출력하도록.
+                    Toast.makeText(Graph.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        //체중 저장값 출력 버튼
+        outputWeightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LineGraphSeries<DataPoint> series = new LineGraphSeries < > (new DataPoint[] {
+                        //그래프 값 입력 시 오름차순 입력할 것, 그렇지 않을 경우 예외처리에 걸려서 오류 메시지 토스트 형식으로 출력됨
+                        new DataPoint(0, 4.4 ),
+                        new DataPoint(Double.valueOf("1"), Double.valueOf("6.5")),
+                        new DataPoint(Double.valueOf("2"), Double.valueOf("7.0")),
+                        new DataPoint(Double.valueOf("3"), Double.valueOf("7.4")),
+                        new DataPoint(Double.valueOf("4"), Double.valueOf("7.8"))
+                });
+                graphWeight.addSeries(series);
             }
         });
 
         //이전 액티비티에서 전달한 값 불러오기
         Intent intent = getIntent();
+
         cm = intent.getExtras().getString("cm").toString();
         kg = intent.getExtras().getString("kg").toString();
         month2 = intent.getExtras().getString("month2").toString();
 
-        TextView txtResult = (TextView) findViewById(R.id.txtResult);
+        txtResult.setText("키 :  " + cm + "\n몸무게 : " + kg + "\n개월수 : " + month2);
 
-        txtResult.setText("키 :  " + cm + "\n 몸무게 : " + kg + "\n 개월수 : " + month2);
-
-        Button btnOk = (Button) findViewById(R.id.btnOk);
-        btnOk.setOnClickListener(new View.OnClickListener() {
-
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                process();
                 finish();
             }
         });
     }
-
-    private void process() {
-        //호출한 액티비티에게 결과값 전달
-        Intent intent = new Intent(this, Input.class);
-        intent.putExtra("cm", cm);
-        intent.putExtra("kg", kg);
-        intent.putExtra("month2", month2);
-        setResult(Activity.RESULT_OK, intent);
-    }
-
 }
--- workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Table.java
+++ workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/Table.java
@@ -3,6 +3,8 @@ package com.capstonedesign.lovebaby;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Table extends Activity {
@@ -14,7 +16,8 @@ public class Table extends Activity {
     TextView weightText_4, heightText_4;
     TextView weightText_5, heightText_5;
 
-    //표 제작 예정
+    Button btnBack;
+    //표 제작
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,39 +30,44 @@ public class Table extends Activity {
         weightText_4 = findViewById(R.id.weightText_4); heightText_4 = findViewById(R.id.heightText_4);
         weightText_5 = findViewById(R.id.weightText_5); heightText_5 = findViewById(R.id.heightText_5);
 
+        //버튼 할당
+        btnBack = findViewById(R.id.btnBack);
+
         //인텐트로 값 할당
         Intent getIntent = getIntent();
         String heightText = getIntent.getExtras().getString("cm");
         String weightText = getIntent.getExtras().getString("kg");
         String monthInput = getIntent.getExtras().getString("month2");
 
+        //intent로 넘어온 개월수에 맞춰서 값이 입력되도록.
         switch (Integer.parseInt(monthInput)) {
             case 1:
                 weightText_1.setText(weightText);
                 heightText_1.setText(heightText);
                 break;
-            case 2:
-            case 3:
+            case 2: case 3:
                 weightText_2.setText(weightText);
                 heightText_2.setText(heightText);
                 break;
-            case 4:
-            case 5:
+            case 4: case 5:
                 weightText_3.setText(weightText);
                 heightText_3.setText(heightText);
                 break;
-            case 6:
-            case 7:
-            case 8:
+            case 6: case 7: case 8:
                 weightText_4.setText(weightText);
                 heightText_4.setText(heightText);
                 break;
-            case 9:
-            case 10:
-            case 11:
+            case 9: case 10: case 11:
                 weightText_5.setText(weightText);
                 heightText_5.setText(heightText);
                 break;
         }
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
