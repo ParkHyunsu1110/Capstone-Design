fda89380e3bbdfe3cb95c6d516a0ccd5f41296b8
HeoYurim <mh10v2@naver.com>
2021-05-22 19:56:20 +0900

    그래프 변경, 테이블 값 수정, 예방접종 수정, 보고서 수정

--- workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/vaccination.java
+++ workspace/LoveBaby/app/src/main/java/com/capstonedesign/lovebaby/vaccination.java
@@ -1,9 +1,8 @@
 package com.capstonedesign.lovebaby;
 
 import android.app.Activity;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,11 +13,10 @@ import android.widget.ListView;
 import android.widget.Toast;
 import java.util.ArrayList;
 
-
-
 public class vaccination extends  Activity {
 
-    ListView listView;
+    ListView listView,listView2;
+    Button vaccinationBtn, btnBack2;
 
 
     @Override
@@ -26,10 +24,15 @@ public class vaccination extends  Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vaccination);  //현재 사용 xml = vaccination.xml
         final ArrayList<String> midList = new ArrayList<String>();
+        final ArrayList<String> midList2 = new ArrayList<String>();
         listView = findViewById(R.id.listView);
+        listView2 = findViewById(R.id.listView2);
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, midList);
         listView.setAdapter(adapter);
+
+        final ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, midList2);
+        listView2.setAdapter(adapter2);
 
         final EditText edtItem = (EditText) findViewById(R.id.MTtext);
         Button btnAdd = findViewById(R.id.addbtn);
@@ -49,6 +52,46 @@ public class vaccination extends  Activity {
                 adapter.notifyDataSetChanged();
 
                 return false;
+            }
+        });
+
+        final EditText edtItem2 = (EditText) findViewById(R.id.MTtext2);
+        Button btnAdd2 = findViewById(R.id.addbtn2);
+        btnAdd2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "메모입력.", Toast.LENGTH_SHORT).show();
+                midList2.add(edtItem2.getText().toString());
+                adapter2.notifyDataSetChanged();
+            }
+        });
+        listView2.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "메모삭제.", Toast.LENGTH_SHORT).show();
+                midList2.remove(position);
+                adapter2.notifyDataSetChanged();
+
+                return false;
+            }
+        });
+
+        vaccinationBtn = findViewById(R.id.vaccinationBtn);
+
+        vaccinationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://nip.kdca.go.kr/irgd/index.html"));
+                startActivity(intent);
+            }
+        });
+
+        btnBack2 = findViewById(R.id.btnBack2);
+
+        btnBack2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
